ea0cfdaf3a120a88179968b2f635428578dc26b6
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-19 06:47:04 +0600

    Initial commit

--- ConcurrencyDemo/ContentView.swift
+++ ConcurrencyDemo/ContentView.swift
@@ -9,18 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            doSomething()
+        } label: {
+            Text("Do Something")
+                .modifier(StandardTextTitle(backgroundColor: .red))
         }
-        .padding()
+
+    }
+    func doSomething(){
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+//Custom Modifier for Text
+struct StandardTextTitle: ViewModifier {
+    
+    @State var backgroundColor: Color? = .white
+    @State var fontName: String? = "Open Sans"
+    @State var fontSize: CGFloat? = 18
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.custom(fontName!, size: fontSize!))
+            .padding(5)
+            .background(backgroundColor)
+            .border(Color.gray)
+            .shadow(color: Color.black, radius: 6, x: 0, y: 5)
+            .lineLimit(1)
     }
 }
