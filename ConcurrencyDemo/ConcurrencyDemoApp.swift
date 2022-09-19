675e64ea2ed1c24e0c2d26d04bdf25feecf7f4f2
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-19 06:26:42 +0600

    Initial Commit

---
+++ ConcurrencyDemo/ConcurrencyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyDemoApp.swift
+//  ConcurrencyDemo
+//
+//  Created by JOYNAL ABEDIN on 19/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
